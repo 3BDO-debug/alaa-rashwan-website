1b94b09e39089ce6fde8ee494dfc9338627123e2
Abdelrahman Ali <aliboody8@gmail.com>
2025-06-15 20:57:52 +0300

    herobanner edit

--- src/sections/homePage/HeroBanner.js
+++ src/sections/homePage/HeroBanner.js
@@ -1,35 +1,55 @@
 "use client";
 import React from "react";
-// mui
-import { Box, useMediaQuery } from "@mui/material";
+import {
+  Box,
+  Button,
+  Container,
+  Typography,
+  useMediaQuery,
+} from "@mui/material";
+import heroBanner from "@/assets/heroBanner.png";
 
 function HeroBanner() {
   const isMd = useMediaQuery((theme) => theme.breakpoints.up("md"));
+
   return (
     <Box
       sx={{
-        height: "100vh",
+        height: "90vh",
         width: "100%",
         overflow: "hidden",
+        backgroundImage: `linear-gradient(to bottom, rgba(3, 98, 146, 0.7), rgba(41, 63, 86, 0.7)), url(${heroBanner.src})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        display: "flex",
+        alignItems: "center",
       }}
     >
-      <video
-        autoPlay
-        loop
-        muted
-        playsInline
-        style={{
-          width: "100%",
-          height: "100%",
-          objectFit: "cover",
-        }}
-      >
-        <source
-          src="https://www.w3schools.com/html/mov_bbb.mp4"
-          type="video/mp4"
-        />
-        Your browser does not support the video tag.
-      </video>
+      <Container maxWidth="xl">
+        <Box sx={{ width: "fit-content" }}>
+          <Typography variant="h2" color="grey.0" fontWeight="bold">
+            REFRESH YOUR
+          </Typography>
+          <Typography variant="h2" color="grey.0" fontWeight="bold">
+            ROUTINE SHAPE
+          </Typography>
+          <Typography variant="h2" color="grey.0" fontWeight="bold">
+            YOUR FITNESS
+          </Typography>
+          <Box sx={{ display: "flex", justifyContent: "center" }}>
+            <Button
+              variant="contained"
+              color="primary"
+              sx={{ mt: 2, borderRadius: 5, width: "60%" }}
+              fullWidth
+              size="large"
+            >
+              Join now
+            </Button>
+          </Box>
+        </Box>
+      </Container>
     </Box>
   );
 }
